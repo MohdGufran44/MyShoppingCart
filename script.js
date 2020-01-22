8be4933fd3c5a4fe10f0eb308322cfed4bc37e42
MohdGufran <gufran.mohd@india.semanticbits.com>
2020-01-22 19:41:20 +0530

    myShoppingCart task Star rating commit.

--- script.js
+++ script.js
@@ -3,9 +3,10 @@ $(function () {
     $.getJSON("sample-data.json",function (data) {
         temData=data;
         for(let i=0; i<temData.length;i++){
+            let di=i+"h";
             if(temData[i].rating===4){
-                $("#dt").append("<div class=' c col-sm-4'><img src='images/"+temData[i].imgPath+".jpeg' width='300px' height='300px'></div>");
-                $(".c").append("<p>"+temData[i].name+"</p>");
+                $("#dt").append("<div class=' c col-sm-4' id='h"+i+"'><img src='images/"+temData[i].imgPath+".jpeg' width='300px' height='300px'></div>");
+                $("#h"+i+"").append("<p>"+temData[i].name+"<br><button class='btn btn-success' style='border-radius: 35%'>"+temData[i].rating+" <i class='fa fa-star-o'></i>","</button>","</p>");
             }
         }
     });
@@ -16,8 +17,8 @@ $(function () {
         let checkTye="ELEC";
         for(let i=0; i<temData.length;i++){
             if(checkTye===temData[i].id.substr(0,4)){
-                $("#dt").append("<div class=' c col-sm-4'><img src='images/"+temData[i].imgPath+".jpeg' width='300px' height='300px'></div>");
-                $(".c").append("<p>"+temData[i].name+"</p>");
+                $("#dt").append("<div class=' c col-sm-4' id='h"+i+"'><img src='images/"+temData[i].imgPath+".jpeg' width='300px' height='300px'></div>");
+                $("#h"+i+"").append("<p>"+temData[i].name+"<br><button class='btn btn-success' style='border-radius: 35%'>"+temData[i].rating+" <i class='fa fa-star-o'></i>","</button>","</p>");
             }
 
         }
@@ -28,8 +29,8 @@ $(function () {
         let checkTye="BOOK";
         for(let i=0; i<temData.length;i++){
             if(checkTye===temData[i].id.substr(0,4)){
-                $("#dt").append("<div class=' c col-sm-4'><img src='images/"+temData[i].imgPath+".jpeg' width='300px' height='300px'></div>");
-                $(".c").append("<p>"+temData[i].name+"</p>");
+                $("#dt").append("<div class=' c col-sm-4' id='h"+i+"'><img src='images/"+temData[i].imgPath+".jpeg' width='300px' height='300px'></div>");
+                $("#h"+i+"").append("<p>"+temData[i].name+"<br><button class='btn btn-success' style='border-radius: 35%'>"+temData[i].rating+" <i class='fa fa-star-o'></i>","</button>","</p>");
             }
 
         }
