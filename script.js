e684910859c1ac2e8eaff6ed98322e58692b7a4d
MohdGufran <gufran.mohd@india.semanticbits.com>
2020-01-24 19:22:37 +0530

    myShoppingCart task  commit on 24-01-2020.

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 $(function () {
 
     let temData;
+
     $.getJSON("sample-data.json",function (data) {
         temData=data;
+        console.log(temData);
         for(let i=0; i<temData.length;i++){
             if(temData[i].rating===4){
                 $("#dt").append("<div class=' c col-sm-4' id='"+temData[i].id+"'><a href='#' target=''><img src='images/"+temData[i].imgPath+".jpeg' width='300px' height='300px' id='"+temData[i].id+"'></a></div>");
@@ -39,19 +41,19 @@ $(function () {
         $("#dt").empty();
         let b=$(this).attr("id");
         console.log(b);
-        $("#dt").append("<div ><img src='images/"+b+".jpeg' width='300px' height='300px'></div>");
+        $("#dt").append("<div style='margin-left: 50px'><img src='images/"+b+".jpeg' width='300px' height='300px'></div>");
         for(let i=0; i<temData.length;i++){
             if(b===temData[i].id){
-                $("#dt").append("<div >" +
-                    "<p>Name: "+temData[i].name+"</p>" +
-                    "<p>Price: "+temData[i].price+"</p>" +
-                    "<p>Comments:</p>" +
-                    "<p>"+temData[i].comments[0].text+"</p>" +
-                    "<p>"+temData[i].comments[1].rating+"</p>" +
-                    "<p>"+temData[i].comments[2].commentedOn+"</p>" +
+                $("#dt").append("<div style='text-align: left; class='data'>" +
+                    "<p style='font-size: 2.5em'>"+temData[i].name+"("+temData[i].color+", "+temData[i].RAM+" RAM)</p>" +
+                    "<p><button class='btn btn-success' style='border-radius: 35%'>"+temData[i].rating+" <i class='fa fa-star-o'></i></button></p>" +
+                    "<p style='font-size: 1.9em'><i class='fa fa-rupee'></i> "+temData[i].price+"</p>" +
+                    "<p>"+temData[i].brand+"</p>" +
+                    "<p>"+temData[i].rating+"</p>" +
+                    "<p>"+temData[i].battery+"</p>" +
                     "</div>");
             }
         }
     });
-
 });
+//<button class='btn btn-success' style='border-radius: 35%'>"+temData[i].rating+" <i class='fa fa-star-o'></i>","</button>"
