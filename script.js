1a09bf341f07a3a31c7d606f25c7edab7455c329
MohdGufran <gufran.mohd@india.semanticbits.com>
2020-01-27 19:36:50 +0530

    update myShoppingCart task   commit on 27-01-2020.

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ $(function () {
 
     $.getJSON("sample-data.json",function (data) {
         temData=data;
-        console.log(temData);
+        console.log(temData[0].comments[0].commentedOn);
         for(let i=0; i<temData.length;i++){
             if(temData[i].rating===4){
                 $("#dt").append("<div class=' c col-sm-4' id='"+temData[i].id+"'><a href='#' target=''><img src='images/"+temData[i].imgPath+".jpeg' width='300px' height='300px' id='"+temData[i].id+"'></a></div>");
@@ -41,19 +41,52 @@ $(function () {
         $("#dt").empty();
         let b=$(this).attr("id");
         console.log(b);
-        $("#dt").append("<div style='margin-left: 50px'><img src='images/"+b+".jpeg' width='300px' height='300px'></div>");
-        for(let i=0; i<temData.length;i++){
-            if(b===temData[i].id){
-                $("#dt").append("<div style='text-align: left; class='data'>" +
+        if(b.substr(0,4)==='ELEC'){
+            $("#dt").append("<div style='margin-left: 20px'><img src='images/"+b+".jpeg' width='300px' height='300px'>" +
+                "<br><button type='button' class='pmbt btn btn-warning'>ADD TO CART</button><button type='button' class='pmbt btn btn-warning' style='margin-left: 20px'>Buy Now</button></div>");
+
+        }
+        else if(b.substr(0,4)==='BOOK'){
+            $("#dt").append("<div style='margin-left: 20px'><img src='images/"+b+".jpeg' width='300px' height='300px'>" +
+                "<br><button type='button' class='pmbt btn btn-warning'>ADD TO CART</button><button type='button' class='pmbt btn btn-warning' style='margin-left: 20px'>Buy Now</button></div>");
+
+        }
+         for(let i=0; i<temData.length;i++){
+             let disc;
+            if(b===temData[i].id&&b.substr(0,4)==='ELEC'){
+                if(temData[i].offers[0].percentage!=null){
+                    disc=temData[i].offers[0].percentage;
+                }else{
+                    disc=" <i class='fa fa-rupee'></i> "+temData[i].offers[0].amount;
+                }
+                $("#dt").append("<div style='text-align: left'; class='data'>" +
                     "<p style='font-size: 2.5em'>"+temData[i].name+"("+temData[i].color+", "+temData[i].RAM+" RAM)</p>" +
                     "<p><button class='btn btn-success' style='border-radius: 35%'>"+temData[i].rating+" <i class='fa fa-star-o'></i></button></p>" +
                     "<p style='font-size: 1.9em'><i class='fa fa-rupee'></i> "+temData[i].price+"</p>" +
-                    "<p>"+temData[i].brand+"</p>" +
-                    "<p>"+temData[i].rating+"</p>" +
-                    "<p>"+temData[i].battery+"</p>" +
+                    "<p> <b>Available offers</b> "+disc+"</p>" +
+                    "<p> Highlights<br><ul><li>"+temData[i].RAM+" | "+temData[i].battery+"</li>"+temData[i].camera.front+" | "+temData[i].camera.front+"</ul> </p>" +
                     "</div>");
+                $("#dt").append("<div class='rev'><p>"+temData[i].description+"</p><b>Reviews</b><br><p><button class='btn btn-success' style='border-radius: 35%'>"+temData[i].comments[0].rating+" <i class='fa fa-star-o'></i></button> <b>"+temData[0].comments[0].username+"</b> <br>" +
+                    ""+temData[i].comments[0].text+"   "+temData[i].comments[0].commentedOn+"</p></div>");
             }
+            else if(b===temData[i].id&&b.substr(0,4)==='BOOK'){
+                let disc;
+                if(temData[i].offers[0].percentage!=null){
+                    disc=temData[i].offers[0].percentage;
+                }else{
+                    disc=" <i class='fa fa-rupee'></i> "+temData[i].offers[0].amount;
+                }
+                $("#dt").append("<div style='text-align: left'; class='data'>" +
+                    "<p style='font-size: 2.5em'>"+temData[i].name+"</p>" +
+                    "<p><button class='btn btn-success' style='border-radius: 35%'>"+temData[i].rating+" <i class='fa fa-star-o'></i></button></p>" +
+                    "<p style='font-size: 1.9em'><i class='fa fa-rupee'></i> "+temData[i].price+"</p>" +
+                    "<p> <b>Available offers</b> "+disc+"</p>" +
+                    "<p> Highlights<br><ul><li>"+temData[i].subType+" | "+temData[i].by+"</li></ul> </p>" +
+                    "</div>");
+                $("#dt").append("<div class='rev'><p>"+temData[i].description+"</p><b>Reviews</b><br><p><button class='btn btn-success' style='border-radius: 35%'>"+temData[i].comments[0].rating+" <i class='fa fa-star-o'></i></button> <b>"+temData[0].comments[1].username+"</b> <br>" +
+                    ""+temData[i].comments[0].text+"   "+temData[i].comments[0].commentedOn+"</p></div>");
+            }
+            console.log(temData[i].comments.length);
         }
     });
 });
-//<button class='btn btn-success' style='border-radius: 35%'>"+temData[i].rating+" <i class='fa fa-star-o'></i>","</button>"
